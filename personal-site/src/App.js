a518b4df011d86483b38ab1f71387585a68f8a9d
RileyDMK <rileydmathis@gmail.com>
2017-11-12 18:12:37 -0800

    Add Navbar

--- personal-site/src/App.js
+++ personal-site/src/App.js
@@ -2,18 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-var todos = [
+var navItems = [
   {
-    todoTitle: 'todo1',
-    todoResponsible: 'Me',
-    todoDescription: 'first todo',
-    todoPriority: 'high'
+    name: 'Home',
+    url: '#',
+    alt: 'Home button',
   },
   {
-    todoTitle: 'todo1',
-    todoResponsible: 'Me',
-    todoDescription: 'first todo',
-    todoPriority: 'high'
+    name: 'About',
+    url: '#',
+    alt: 'About button',
+  },
+  {
+    name: 'Projects',
+    url: '#',
+    alt: 'Projects button',
+  },
+  {
+    name: 'Contact',
+    url: '#',
+    alt: 'Contact button',
   }
 ]
 
@@ -21,21 +29,54 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      todos
+      navItems
     };
   }
   render() {
     return (
       <div className="container">
-        <h4>Todo Count: <span className="badge badge-secondary">{this.state.todos.length}</span></h4>
-        <ul className="list-group">
+        {/* <h4>Todo Count: <span className="badge badge-secondary">{this.state.todos.length}</span></h4>
+          <ul className="list-group">
           {this.state.todos.map((todo, index) =>
             <li className="list-group-item" key={index}>
-              <h4 className="list-group-item-heading">{todo.todoTitle}</h4>
+          <h4 className="list-group-item-heading">{todo.todoTitle}</h4>
             </li>
           )}
-        </ul>
+        </ul> */}
+        <Nav></Nav>
       </div>
+    );
+  }
+}
+
+class Nav extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      navItems
+    }
+  }
+  render(){
+    return(
+      <nav className="navbar navbar-expand-lg fixed-bottom navbar-light bg-light">
+        <a className="navbar-brand" href="#">Riley Mathis</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            {this.state.navItems.map((item, index) =>
+              index===0 ? (
+                <li className="nav-item active">
+                  <a className="nav-link" href="{item.url}">{item.name}<span className="sr-only">(current)</span></a>
+                </li>) : (
+                  <li className="nav-item">
+                    <a className="nav-link" href="{item.url}">{item.name}<span className="sr-only">(current)</span></a>
+                  </li>)
+            )}
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
